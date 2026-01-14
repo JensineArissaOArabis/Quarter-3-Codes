a87643e83386f0a94a6962f018a2fecff2fb782a
JensineArissaOArabis <jensinearissa.arabis@smc.pshs.edu.ph>
2026-01-14 22:09:23 +0800

    Arabis_Q3_FA03.py
    This file contains the codes for FA3, Activity 2.

---
+++ Arabis_Q3_FA03.py
@@ -0,0 +1,11 @@
+import numpy as jn
+
+names = ["Me", "Lia", "Jake"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+steps = jn.array([[4500,200,4800,5000,5300],
+                   [4000,4100,3900,4200,4600],
+                   [6000,5800,5900,6100,6200]])
+
+for i in range(len(names)):
+    print(names[i], ":", "Total steps:", jn.sum(steps[i]), "|", "Average:", jn.mean(steps[i]), "|", "Min:", jn.min(steps[i]), "|", "Max:", jn.max(steps[i]))
